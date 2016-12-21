e8fb14b1d12bdcb5eedc546022cf166d8c4ad330
Tanner Nelson <me@tanner.xyz>
2016-12-21 13:29:53 -0500

    parse multipart working

--- Sources/Multipart/Parser.swift
+++ Sources/Multipart/Parser.swift
@@ -14,105 +14,244 @@ public final class Parser {
     public typealias PreambleCallback = (Bytes) -> ()
     public var onPreamble: PreambleCallback?
     
-    public typealias EpilogueUpdateCallback = (Bytes) -> ()
-    public var onEpilogueUpdate: EpilogueUpdateCallback?
+    public typealias EpilogueCallback = (Bytes) -> ()
+    public var onEpilogue: EpilogueCallback?
     
     private enum PartState {
         case headers
         case body
     }
     
+    public enum Error: Swift.Error {
+        case hasAlreadyFinished
+    }
+    
     private enum State {
-        case preamble(body: Bytes)
+        case preamble(
+            bodyEndIndex: Int
+        )
         case part(
             state: PartState,
             headers: [String: String],
-            body: Bytes
+            bodyEndIndex: Int
         )
-        case epilogue(body: Bytes)
+        case epilogue
     }
+    
     private var state: State
+    
+    private var boundaryParser: BoundaryParser
+    
+    private var headerParser: HeaderParser
 
 	public init(boundary: Bytes) {
 		self.boundary = boundary
-        state = .preamble(body: [])
-        boundaryState = .none
+        state = .preamble(bodyEndIndex: 0)
+        
+        boundaryParser = BoundaryParser(boundary: boundary)
+        headerParser = HeaderParser()
+        
+        buffer = []
+        hasFinished = false
 	}
     
     public convenience init(boundary: String) {
         self.init(boundary: boundary.bytes)
     }
     
-    public func parse(_ bytes: Bytes) {
+    private var buffer: Bytes
+    
+    public func parse(_ bytes: Bytes) throws {
+        buffer += bytes
+        
         var i = bytes.makeIterator()
         while let byte = i.next() {
-            parse(byte)
+            try parse(byte)
         }
     }
     
-    public func parse(_ byte: Byte) {
+    public func parse(_ byte: Byte) throws {
+        guard !hasFinished else {
+            throw Error.hasAlreadyFinished
+        }
+        
         switch state {
-        case .preamble(var body):
-            
-            parseBoundary(byte)
-            switch boundaryState {
+        case .preamble(let bodyEndIndex):
+            try boundaryParser.parse(byte)
+            switch boundaryParser.state {
             case .none:
-                body.append(byte)
-                state = .preamble(body: body)
+                state = .preamble(bodyEndIndex: bodyEndIndex + 1)
             case .parsing:
                 break
             case .invalid(let failed):
-                body += failed
-                state = .preamble(body: body)
-            case .detected(let closing):
+                state = .preamble(bodyEndIndex: bodyEndIndex + failed.count)
+            case .finished(let boundarySize, let closing):
                 if closing {
-                    state = .epilogue(body: [])
+                    state = .epilogue
                 } else {
-                    state = .part(state: .headers, headers: [:], body: [])
+                    state = .part(state: .headers, headers: [:], bodyEndIndex: 0)
                 }
+                
+                let body = Array(buffer[0..<bodyEndIndex])
+                
+                let pos = bodyEndIndex + boundarySize
+                buffer = Array(buffer[pos..<buffer.count])
+                
                 onPreamble?(body)
             }
-        case .part(_, let headers, var body):
-            parseBoundary(byte)
-            switch boundaryState {
-            case .none:
-                body.append(byte)
-                state = .part(state: .headers, headers: [:], body: body)
-            case .parsing:
-                break
-            case .invalid(let failed):
-                body += failed
-                state = .part(state: .headers, headers: [:], body: body)
-            case .detected(let closing):
-                if closing {
-                    state = .epilogue(body: [])
-                } else {
-                    state = .part(state: .headers, headers: [:], body: [])
+        case .part(let partState, var headers, let bodyEndIndex):
+            switch partState {
+            case .headers:
+                try headerParser.parse(byte)
+                switch headerParser.state {
+                case .parsingKey:
+                    break
+                case .parsingValue:
+                    break
+                case .finished(let key, let value):
+                    headers[key.trimmed([.space]).string] = value.trimmed([.space]).string
+                    
+                    let pos = key.count + 1 + value.count + 1
+                    buffer = Array(buffer[pos..<buffer.count])
+                    
+                    state = .part(
+                        state: .headers,
+                        headers: headers,
+                        bodyEndIndex: 0
+                    )
+                case .none:
+                    buffer = Array(buffer[1..<buffer.count])
+                    
+                    state = .part(
+                        state: .body,
+                        headers: headers,
+                        bodyEndIndex: 0
+                    )
                 }
-                let part = Part(headers: headers, body: body)
-                onPart?(part)
+            case .body:
+                try boundaryParser.parse(byte)
+                switch boundaryParser.state {
+                case .none:
+                    state = .part(
+                        state: .body,
+                        headers: headers,
+                        bodyEndIndex: bodyEndIndex + 1
+                    )
+                case .parsing:
+                    break
+                case .invalid(let failed):
+                    state = .part(
+                        state: .body,
+                        headers: headers,
+                        bodyEndIndex: bodyEndIndex + failed.count
+                    )
+                case .finished(let boundarySize, let closing):
+                    if closing {
+                        state = .epilogue
+                    } else {
+                        state = .part(state: .body, headers: headers, bodyEndIndex: 0)
+                    }
+                    
+                    let body = Array(buffer[0..<bodyEndIndex])
+                    
+                    let pos = bodyEndIndex + boundarySize
+                    buffer = Array(buffer[pos..<buffer.count])
+                    
+                    let part = Part(headers: headers, body: body)
+                    onPart?(part)
+                }
             }
-        case .epilogue(var body):
-            body.append(byte)
-            state = .epilogue(body: body)
-            onEpilogueUpdate?(body)
+        case .epilogue:
+            break
         }
     }
     
-    private enum BoundaryState {
+    private var hasFinished: Bool
+    
+    public func finish() throws {
+        guard !hasFinished else {
+            throw Error.hasAlreadyFinished
+        }
+        
+        hasFinished = true
+        let body = buffer
+        buffer = []
+        onEpilogue?(body)
+    }
+}
+
+final class HeaderParser {
+    enum State {
+        case none
+        case parsingKey(buffer: Bytes)
+        case parsingValue(key: Bytes, buffer: Bytes)
+        case finished(key: Bytes, value: Bytes)
+    }
+    
+    var state: State
+    
+    init() {
+        self.state = .none
+    }
+    
+    func parse(_ byte: Byte) throws {
+        main: switch state {
+        case .none:
+            if byte == .newLine {
+                break main
+            }
+            
+            state = .parsingKey(buffer: [byte])
+        case .parsingKey(var buffer):
+            if byte == .colon {
+                state = .parsingValue(key: buffer, buffer: [])
+                break main
+            }
+            
+            buffer.append(byte)
+            state = .parsingKey(buffer: buffer)
+        case .parsingValue(let key, var buffer):
+            if byte == .newLine {
+                state = .finished(key: key, value: buffer)
+                break main
+            }
+            
+            buffer.append(byte)
+            state = .parsingValue(key: key, buffer: buffer)
+        case .finished:
+            if byte == .newLine {
+                state = .none
+                break main
+            }
+            
+            state = .parsingKey(buffer: [byte])
+        }
+    }
+}
+
+final class BoundaryParser {
+    enum State {
         case none
         case parsing(buffer: Bytes, trailingHyphenCount: Int)
         case invalid(failed: Bytes)
-        case detected(closing: Bool)
+        case finished(boundarySize: Int, closing: Bool)
     }
     
-    private var boundaryState: BoundaryState
+    let boundary: Bytes
     
-    private func parseBoundary(_ byte: Byte) {
-        main: switch boundaryState {
+    var state: State
+    
+    init(boundary: Bytes) {
+        self.boundary = boundary
+        self.state = .none
+    }
+    
+    
+    func parse(_ byte: Byte) throws {
+        main: switch state {
         case .none:
             if byte == .hyphen {
-                boundaryState = .parsing(buffer: [byte], trailingHyphenCount: 0)
+                state = .parsing(buffer: [byte], trailingHyphenCount: 0)
                 break main
             }
         case .parsing(let buffer, let trailingHyphenCount):
@@ -120,35 +259,45 @@ public final class Parser {
             
             if
                 (buffer.count <= 1 && byte == .hyphen) ||
-                (buffer.count > 1 && buffer.count < match.count)
+                    (buffer.count > 1 && buffer.count < match.count)
             {
-                boundaryState = .parsing(buffer: buffer + [byte], trailingHyphenCount: trailingHyphenCount)
+                state = .parsing(buffer: buffer + [byte], trailingHyphenCount: trailingHyphenCount)
                 break main
             } else {
                 if buffer == match {
                     if byte == .newLine {
                         switch trailingHyphenCount {
                         case 0:
-                            boundaryState = .detected(closing: false)
+                            // --boundary + \n
+                            let size = match.count + 1
+                            state = .finished(
+                                boundarySize: size,
+                                closing: false
+                            )
                             break main
                         case 2:
-                            boundaryState = .detected(closing: true)
+                            // --boundary + -- + \n
+                            let size = match.count + 2 + 1
+                            state = .finished(
+                                boundarySize: size,
+                                closing: true
+                            )
                             break main
                         default:
                             break
                         }
                     } else if byte == .hyphen {
-                        boundaryState = .parsing(buffer: buffer, trailingHyphenCount: trailingHyphenCount + 1)
+                        state = .parsing(buffer: buffer, trailingHyphenCount: trailingHyphenCount + 1)
                         break main
                     }
                 }
             }
-        
-            boundaryState = .invalid(failed: buffer + [byte])
+            
+            state = .invalid(failed: buffer + [byte])
         case .invalid:
-            boundaryState = .none
-        case .detected:
-            boundaryState = .none
+            state = .none
+        case .finished:
+            state = .none
         }
     }
 }
--- Tests/MultipartTests/ParserTests.swift
+++ Tests/MultipartTests/ParserTests.swift
@@ -14,7 +14,7 @@ class ParserTests: XCTestCase {
         XCTAssertEqual(parser.boundary, "foo".bytes)
     }
     
-    func testPreamble() {
+    func testPreamble() throws {
         let parser = Parser(boundary: "foo")
         
         var preamble = ""
@@ -34,12 +34,12 @@ class ParserTests: XCTestCase {
             preambleExpectation.fulfill()
         }
         
-        parser.parse(message.bytes)
+        try parser.parse(message.bytes)
         
         waitForExpectations(timeout: 0, handler: nil)
     }
     
-    func testPreambleWithFauxBoundaries() {
+    func testPreambleWithFauxBoundaries() throws {
         let parser = Parser(boundary: "foo")
         
         var preamble = ""
@@ -59,12 +59,12 @@ class ParserTests: XCTestCase {
             preambleExpectation.fulfill()
         }
         
-        parser.parse(message.bytes)
+        try parser.parse(message.bytes)
         
         waitForExpectations(timeout: 0, handler: nil)
     }
     
-    func testParts() {
+    func testParts() throws {
         let parser = Parser(boundary: "foo")
         
         var preamble = ""
@@ -78,6 +78,7 @@ class ParserTests: XCTestCase {
         var message = ""
         message += preamble
         message += "--foo\n"
+        message += "\n"
         message += part1
         message += "--foo--\n"
         
@@ -88,7 +89,64 @@ class ParserTests: XCTestCase {
             partExpectation.fulfill()
         }
         
-        parser.parse(message.bytes)
+        try parser.parse(message.bytes)
+        
+        waitForExpectations(timeout: 0, handler: nil)
+    }
+    
+    func testHeaders() throws {
+        let parser = Parser(boundary: "foo")
+        
+        let part1 = "testfoobar"
+        
+        var message = ""
+        message += "preamble"
+        message += "--foo\n"
+        message += "key:  value\n"
+        message += "foo:bar\n"
+        message += "\n"
+        message += part1
+        message += "--foo--\n"
+        
+        let partExpectation = expectation(description: "part was parsed")
+        
+        parser.onPart = { part in
+            XCTAssertEqual(part.body.string, part1)
+            XCTAssertEqual(part.headers.count, 2)
+            XCTAssertEqual(part.headers["key"], "value")
+            XCTAssertEqual(part.headers["foo"], "bar")
+            partExpectation.fulfill()
+        }
+        
+        try parser.parse(message.bytes)
+        
+        waitForExpectations(timeout: 0, handler: nil)
+    }
+    
+    func testEpilogue() throws {
+        let parser = Parser(boundary: "foo")
+        
+        let epilogue = "\nepliogue"
+        
+        var message = ""
+        message += "preamble"
+        message += "--foo\n"
+        message += "\n"
+        message += "part"
+        message += "--foo--\n"
+        message += epilogue
+        
+        let epilogueExpectation = expectation(description: "epilogue was parsed")
+        
+        parser.onEpilogue = { e in
+            XCTAssertEqual(e.string, epilogue)
+            epilogueExpectation.fulfill()
+        }
+        
+        try parser.parse(message.bytes)
+        
+        // must call done since epilogue can go on forever
+        try parser.finish()
         
         waitForExpectations(timeout: 0, handler: nil)
     }
