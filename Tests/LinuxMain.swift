17efdc6a10d9a10af16b7b0e72e4458244b5ab9a
tanner0101 <me@tanner.xyz>
2018-04-12 18:42:52 -0400

    fix linuxmain file

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,16 +1,8 @@
 #if os(Linux)
 
 import XCTest
-@testable import HTTPTests
 @testable import MultipartTests
-@testable import FormURLEncodedTests
 XCTMain([
-    testCase(HTTPClientTests.allTests),
-
-    testCase(FormURLEncodedCodableTests.allTests),
-    testCase(FormURLEncodedParserTests.allTests),
-    testCase(FormURLEncodedSerializerTests.allTests),
-
     testCase(MultipartTests.allTests),
 ])
 
