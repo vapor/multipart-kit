372f9d7a28c40b3a25c151b1c40427fff0320736
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2021-04-24 14:58:37 +0200

    Remove leftover test helper

--- Tests/MultipartKitTests/MultipartKitTests.swift
+++ Tests/MultipartKitTests/MultipartKitTests.swift
@@ -377,29 +377,6 @@ class MultipartTests: XCTestCase {
     }
 }
 
-public struct RepeatingSequence<T: Collection>: Sequence {
-    private let base: T
-    public init(baseCollection: T) {
-        self.base = baseCollection
-    }
-    public func makeIterator() -> AnyIterator<T.Element> {
-        var i = base.startIndex
-        return AnyIterator({ [base] () -> T.Element in
-            defer {
-                i = (base.index(after: i) == base.endIndex)
-                    ? base.startIndex : base.index(after: i)
-            }
-            return base[i]
-        })
-    }
-}
-
-extension Collection {
-    public func repeated() -> RepeatingSequence<Self> {
-        return RepeatingSequence(baseCollection: self)
-    }
-}
-
 // https://stackoverflow.com/a/54524110/1041105
 private extension Collection {
     func chunked(by maxLength: Int) -> [SubSequence] {
