75d2e53ed7646c6e0f4f527e2d9e69c777844090
siemensikkema <siemensikkema@users.noreply.github.com>
2021-09-01 11:04:23 +0200

    Complement and correct API docs

--- Sources/MultipartKit/MultipartSerializer.swift
+++ Sources/MultipartKit/MultipartSerializer.swift
@@ -4,14 +4,9 @@ import struct NIO.ByteBufferAllocator
 ///
 /// See `MultipartParser` for more information about the multipart encoding.
 public final class MultipartSerializer {
+
     /// Creates a new `MultipartSerializer`.
     public init() { }
-
-    public func serialize(parts: [MultipartPart], boundary: String) throws -> String {
-        var buffer = ByteBufferAllocator().buffer(capacity: 0)
-        try self.serialize(parts: parts, boundary: boundary, into: &buffer)
-        return String(decoding: buffer.readableBytesView, as: UTF8.self)
-    }
 
     /// Serializes the `MultipartForm` to data.
     ///
@@ -23,6 +18,23 @@ public final class MultipartSerializer {
     ///     - boundary: Multipart boundary to use for encoding. This must not appear anywhere in the encoded data.
     /// - throws: Any errors that may occur during serialization.
     /// - returns: `multipart`-encoded `Data`.
+    public func serialize(parts: [MultipartPart], boundary: String) throws -> String {
+        var buffer = ByteBufferAllocator().buffer(capacity: 0)
+        try self.serialize(parts: parts, boundary: boundary, into: &buffer)
+        return String(decoding: buffer.readableBytesView, as: UTF8.self)
+    }
+
+    /// Serializes the `MultipartForm` into a `ByteBuffer`.
+    ///
+    ///     var buffer = ByteBuffer()
+    ///     try MultipartSerializer().serialize(parts: [part], boundary: "123", into: &buffer)
+    ///     print(String(buffer: buffer)) // multipart-encoded
+    ///
+    /// - parameters:
+    ///     - parts: One or more `MultipartPart`s to serialize into `Data`.
+    ///     - boundary: Multipart boundary to use for encoding. This must not appear anywhere in the encoded data.
+    ///     - buffer: Buffer to write to.
+    /// - throws: Any errors that may occur during serialization.
     public func serialize(parts: [MultipartPart], boundary: String, into buffer: inout ByteBuffer) throws {
         for part in parts {
             buffer.writeString("--")
