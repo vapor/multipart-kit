b03269ec1053b386d90cb731142ec8d4b3565579
Gwynne Raskind <gwynne@vapor.codes>
2023-06-28 16:36:36 -0500

    Update Exports.swift

--- Sources/MultipartKit/Exports.swift
+++ Sources/MultipartKit/Exports.swift
@@ -1,4 +1,10 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import protocol Foundation.DataProtocol
+@_documentation(visibility: internal) @_exported import struct NIO.ByteBuffer
+@_documentation(visibility: internal) @_exported import struct NIOHTTP1.HTTPHeaders
+
+#else
 
 @_exported import protocol Foundation.DataProtocol
 @_exported import struct NIO.ByteBuffer
