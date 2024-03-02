2d8006b7706e48a899b382fbe8fe20e4f5105b3c
Andrew <8133022+andrew804@users.noreply.github.com>
2024-03-02 13:41:57 +0000

    Import NIOFoundationCompat

--- Package.swift
+++ Package.swift
@@ -22,6 +22,7 @@ let package = Package(
             dependencies: [
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "NIOFoundationCompat", package: "swift-nio"),
                 .product(name: "Collections", package: "swift-collections"),
             ]
         ),
