b23f6c7af9525f1b6c6f0f4c9bb845e2f7381dda
MahdiBM <github@mahdibm.com>
2025-01-13 23:00:15 +0330

    minor fixes

--- Benchmarks/Parser/AsyncSyncSequence.swift
+++ Benchmarks/Parser/AsyncSyncSequence.swift
@@ -1,14 +1,3 @@
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift Async Algorithms open source project
-//
-// Copyright (c) 2022 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-//
-//===----------------------------------------------------------------------===//
-
 extension Array<ArraySlice<UInt8>> {
     var async: AsyncSyncSequence {
         AsyncSyncSequence(self)
