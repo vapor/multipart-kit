91d8b31c77a7c113c19d4ab96395c2e82ff9022e
Nakul <nakulverma.py@gmail.com>
2025-02-04 19:11:25 +0530

    feat: Make MultipartParserError public

--- Sources/MultipartKit/MultipartParserError.swift
+++ Sources/MultipartKit/MultipartParserError.swift
@@ -1,6 +1,6 @@
 /// Technical parsing error, such as malformed data or invalid characters.
 /// This is mainly used by ``MultipartParser``.
-package enum MultipartParserError: Swift.Error, Equatable {
+public enum MultipartParserError: Swift.Error, Equatable {
     case invalidBoundary
     case invalidHeader(reason: String)
     case invalidBody(reason: String)
