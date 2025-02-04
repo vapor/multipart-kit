1ccc071970671e5f73a64a36ea82e888ae62bf06
Nakul <nakulverma.py@gmail.com>
2025-02-04 20:04:36 +0530

    feat: Make MultipartParserError public like JWT

--- Sources/MultipartKit/MultipartParserError.swift
+++ Sources/MultipartKit/MultipartParserError.swift
@@ -1,7 +1,38 @@
 /// Technical parsing error, such as malformed data or invalid characters.
 /// This is mainly used by ``MultipartParser``.
-public enum MultipartParserError: Swift.Error, Equatable {
+/// Protocol for `MultipartParserError`, allowing extensibility without breaking the API.
+public protocol MultipartParserError: Swift.Error, Equatable {}
+
+public struct InvalidBoundaryError: MultipartParserError {
+    public init() {}
+}
+
+public struct InvalidHeaderError: MultipartParserError {
+    public let reason: String
+    
+    public init(reason: String) {
+        self.reason = reason
+    }
+}
+
+public struct InvalidBodyError: MultipartParserError {
+    public let reason: String
+    
+    public init(reason: String) {
+        self.reason = reason
+    }
+}
+
+public enum MultipartParserErrorType {
     case invalidBoundary
-    case invalidHeader(reason: String)
-    case invalidBody(reason: String)
+    case invalidHeader(String)
+    case invalidBody(String)
+}
+
+public struct MultipartParserErrorWrapper: MultipartParserError {
+    public let type: MultipartParserErrorType
+    
+    public init(type: MultipartParserErrorType) {
+        self.type = type
+    }
 }
