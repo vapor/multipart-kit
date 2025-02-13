ca878c9c4fc3f0d9c797fd60783cb86af33de42a
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-02-13 15:58:26 +0100

    Remove indentation

--- Tests/MultipartKitTests/SerializerTests.swift
+++ Tests/MultipartKitTests/SerializerTests.swift
@@ -1,33 +1,33 @@
 #if canImport(Testing)
-    import HTTPTypes
-    import MultipartKit
-    import Testing
+import HTTPTypes
+import MultipartKit
+import Testing
 
-    @Suite("Serializer Tests")
-    struct SerializerTests {
-        @Test("Serialize Example")
-        func serialize() async throws {
-            let example: [MultipartPart] = [
-                .init(
-                    headerFields: .init([
-                        .init(name: .contentDisposition, value: "form-data; name=\"file\"; filename=\"hello.txt\""),
-                        .init(name: .contentType, value: "text/plain"),
-                    ]),
-                    body: ArraySlice("Hello, world!".utf8)
-                )
-            ]
+@Suite("Serializer Tests")
+struct SerializerTests {
+    @Test("Serialize Example")
+    func serialize() async throws {
+        let example: [MultipartPart] = [
+            .init(
+                headerFields: .init([
+                    .init(name: .contentDisposition, value: "form-data; name=\"file\"; filename=\"hello.txt\""),
+                    .init(name: .contentType, value: "text/plain"),
+                ]),
+                body: ArraySlice("Hello, world!".utf8)
+            )
+        ]
 
-            let serialized: ArraySlice<UInt8> = MultipartSerializer(boundary: "boundary123").serialize(parts: example)
-            let expected = ArraySlice(
-                """
-                --boundary123\r
-                Content-Disposition: form-data; name="file"; filename="hello.txt"\r
-                Content-Type: text/plain\r
-                \r
-                Hello, world!\r
-                --boundary123--\r\n
-                """.utf8)
-            #expect(serialized == expected)
-        }
+        let serialized: ArraySlice<UInt8> = MultipartSerializer(boundary: "boundary123").serialize(parts: example)
+        let expected = ArraySlice(
+            """
+            --boundary123\r
+            Content-Disposition: form-data; name="file"; filename="hello.txt"\r
+            Content-Type: text/plain\r
+            \r
+            Hello, world!\r
+            --boundary123--\r\n
+            """.utf8)
+        #expect(serialized == expected)
     }
+}
 #endif  // canImport(Testing)
