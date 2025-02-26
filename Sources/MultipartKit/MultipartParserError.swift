e81442fd25cfa39ffcd84bb1e788042817c3619e
verma-nakul <nakulverma.py@gmail.com>
2025-02-26 15:57:23 +0530

    Make MultipartParserError Public (#112)
    * feat: Make MultipartParserError public
    
    * feat: Make MultipartParserError public like JWT
    
    * feat: Make MultipartParserError public like JWT
    
    * feat: Make MultipartParserError public like JWT
    
    * feat: Making MultipartParserError public like JWT
    
    * Update Sources/MultipartKit/MultipartParserError.swift
    
    Co-authored-by: Mahdi Bahrami <github@mahdibm.com>
    
    * Update Sources/MultipartKit/MultipartParserError.swift
    
    Co-authored-by: Mahdi Bahrami <github@mahdibm.com>
    
    * feat: Making MultipartParserError public
    
    * Making MultipartParserError public
    
    * feat: Make MultipartParserError public
    
    * feat: Make MultipartParserError public
    
    * MultipartParserError message with reason
    
    * feat: Making MultipleParserError public
    
    * Fix build
    
    ---------
    
    Co-authored-by: Mahdi Bahrami <github@mahdibm.com>
    Co-authored-by: Paul Toffoloni <69189821+ptoffy@users.noreply.github.com>
    Co-authored-by: Paul Toffoloni <paultoffoloni@gmail.com>

--- Sources/MultipartKit/MultipartParserError.swift
+++ Sources/MultipartKit/MultipartParserError.swift
@@ -1,7 +1,65 @@
 /// Technical parsing error, such as malformed data or invalid characters.
 /// This is mainly used by ``MultipartParser``.
-package enum MultipartParserError: Swift.Error, Equatable {
-    case invalidBoundary
-    case invalidHeader(reason: String)
-    case invalidBody(reason: String)
+public struct MultipartParserError: Swift.Error, Equatable, Sendable {
+    public struct ErrorType: Sendable, Equatable, CustomStringConvertible {
+        enum Base: String, Equatable {
+            case invalidBoundary
+            case invalidHeader
+            case invalidBody
+        }
+
+        let base: Base
+
+        private init(_ base: Base) {
+            self.base = base
+        }
+
+        public static let invalidBoundary = Self(.invalidBoundary)
+        public static let invalidHeader = Self(.invalidHeader)
+        public static let invalidBody = Self(.invalidBody)
+
+        public var description: String {
+            base.rawValue
+        }
+    }
+
+    private struct Backing: Equatable, Sendable {
+        let errorType: ErrorType
+        let reason: String?
+    }
+
+    private var backing: Backing
+
+    public var errorType: ErrorType { backing.errorType }
+    public var reason: String? { backing.reason }
+
+    private init(backing: Backing) {
+        self.backing = backing
+    }
+
+    private init(errorType: ErrorType) {
+        self.backing = .init(errorType: errorType, reason: nil)
+    }
+
+    public static let invalidBoundary = Self(errorType: .invalidBoundary)
+
+    public static func invalidHeader(reason: String) -> Self {
+        .init(backing: .init(errorType: .invalidHeader, reason: reason))
+    }
+
+    public static func invalidBody(reason: String) -> Self {
+        .init(backing: .init(errorType: .invalidBody, reason: reason))
+    }
+
+    public var description: String {
+        var result = "MultipartParserError(errorType: \(errorType)"
+
+        if let reason {
+            result.append(", reason: \(reason)")
+        }
+
+        result.append(")")
+
+        return result
+    }
 }
