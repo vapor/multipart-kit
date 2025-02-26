46aadbe00156bde5fa16d60bca83ea362e36bd9d
Francesco Paolo Severino <96546612+fpseverino@users.noreply.github.com>
2025-02-26 10:52:55 +0100

    Add Android to CI (#126)
    * Add Android to CI
    
    * Run tests only on platforms where Swift Testing is supported
    
    * Remove indentation
    
    ---------
    
    Co-authored-by: Mahdi Bahrami <github@mahdibm.com>

--- Tests/MultipartKitTests/ParserTests.swift
+++ Tests/MultipartKitTests/ParserTests.swift
@@ -1,3 +1,4 @@
+#if canImport(Testing)
 import HTTPTypes
 import MultipartKit
 import Testing
@@ -269,3 +270,4 @@ struct ParserTests {
         0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82,
     ]
 }
+#endif  // canImport(Testing)
